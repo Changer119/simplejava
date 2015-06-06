62f0c1599282c60b84e965d76cfb3a17144c629b
fcjiang <fachangjiang@qq.com>
2015-06-06 18:40:28 +0800

    实现ApplicationContext产生Bean的功能

--- springlearn/src/main/java/com/changer/springbean/AppMain.java
+++ springlearn/src/main/java/com/changer/springbean/AppMain.java
@@ -1,5 +1,19 @@
 package com.changer.springbean;
 
-public class AppMain {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class AppMain {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(AppMain.class);
+	
+	public static void main(String[] args){
+		// ApplicationContext applicationContext = new ClassPathXmlApplicationContext("appcontext-springbean");
+		@SuppressWarnings("resource")
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/changer/springbean/appcontext-springbean.xml");
+		Person person = (Person) applicationContext.getBean("person");
+		person.sayHello();
+	}
 }
